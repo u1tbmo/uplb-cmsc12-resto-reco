80e45c9c18d35eef6d1c91727dfdaa8acb8ffed7
Euan Jed Tabamo <tabamoeuan@gmail.com>
2023-11-11 17:50:43 +0800

    Add color codes for terminal output

---
+++ colors.py
@@ -0,0 +1,41 @@
+"""
+This file contains the color codes for the terminal output.
+"""
+
+# Text Formatting
+END = "\33[0m"
+BOLD = "\33[1m"
+ITALIC = "\33[3m"
+UNDERLINE = "\33[4m"
+DUNDERLINE = "\33[21m"
+BLINK = "\33[5m"
+BLINK2 = "\33[6m"
+SELECTED = "\33[7m"
+
+# Text Colors
+BLACK = "\33[30m"
+GREY = "\33[90m"
+RED = "\33[31m"
+RED2 = "\33[91m"
+GREEN = "\33[32m"
+GREEN2 = "\33[92m"
+YELLOW = "\33[33m"
+YELLOW2 = "\33[93m"
+BLUE = "\33[34m"
+BLUE2 = "\33[94m"
+VIOLET = "\33[35m"
+VIOLET2 = "\33[95m"
+CYAN = "\33[36m"
+CYAN2 = "\33[96m"
+WHITE = "\33[37m"
+WHITE2 = "\33[97m"
+
+# Background Colors
+BGBLACK = "\33[40m"
+BGRED = "\33[41m"
+BGGREEN = "\33[42m"
+BGYELLOW = "\33[43m"
+BGBLUE = "\33[44m"
+BGVIOLET = "\33[45m"
+BGCYAN = "\33[46m"
+BGWHITE = "\33[47m"
